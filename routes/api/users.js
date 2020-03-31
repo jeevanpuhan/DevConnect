f9da60b62e294153d00429931731d23b1ca57bc8
jeevanpuhan <jeevankumarpuhan@gmail.com>
2020-03-31 22:02:53 +0530

    User Authentication Login Route

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,7 +4,7 @@ const gravatar = require('gravatar')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const config = require('config')
-const { check, validationResult } = require('express-validator/check')
+const { check, validationResult } = require('express-validator')
 // Express-validator-docs:  https://express-validator.github.io/docs/
 
 const User = require('../../models/User') // fetch the user model
