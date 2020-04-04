ac4051778e8b946120ce800174679c6c416c6486
jeevanpuhan <jeevankumarpuhan@gmail.com>
2020-04-04 14:54:09 +0530

    Added route for Delete Profile Experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -225,4 +225,28 @@ router.put(
   }
 )
 
+// @route       DELETE api/Profile/experience/exp_id
+// @desc        Delete experience from profile
+// @access      Private
+
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id })
+
+    // Get remove index page
+    const removeIndex = profile.experience
+      .map(item => item.id)
+      .indexOf(req.params.exp_id)
+
+    profile.experience.splice(removeIndex, 1)
+
+    await profile.save()
+
+    res.json(profile)
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send('Server Error')
+  }
+})
+
 module.exports = router
